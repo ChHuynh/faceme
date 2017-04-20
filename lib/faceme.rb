583ce75ed36e6e29ca2e8aa0f159785440595b61
ChHuynh <christopher.huynh.is@gmail.com>
2017-04-20 14:04:40 -0700

    add files, initial commit to repo

---
+++ lib/faceme.rb
@@ -0,0 +1,66 @@
+require "faceme/version"
+
+module Faceme
+  def self.riot
+    p	"༼ つ ◕_◕ ༽つhello༼ つ ◕_◕ ༽つ"
+  end
+
+  def self.lenny
+  	p "( ͡° ͜ʖ ͡°)"
+  end
+
+  def self.disapproval
+  	p "ಠ_ಠ"
+  end
+
+  def self.shrug
+  	p "¯\\_(ツ)_/¯"
+  end
+
+  def self.tableflip
+  	p "(╯°□°）╯︵ ┻━┻"
+  end
+
+  def self.unflip
+  	p "┬─┬﻿ ノ( ゜-゜ノ)"
+  end
+
+  def self.angry
+  	p "( ≧Д≦)"
+  end
+
+  def self.angry2
+  	p "(＃`Д´)"
+  end
+
+  def self.sweatdrop
+  	p "(；￣Д￣）"
+  end
+
+
+  def self.devious
+  	p "( ` ω ´ )"
+  end
+
+  def self.wave
+  	p "(=ﾟωﾟ)ﾉ"
+  end
+
+  def self.twoface
+  	p "(ﾟДﾟ≡ﾟДﾟ)"
+  end
+
+  def self.angry3
+  	p "(*´>д<)"
+  end
+
+  def self.chill
+  	p "Σ(=ω= ;)"
+  end
+
+  def self.happy
+  	p "(::^ω^::)"
+  end
+
+
+end
